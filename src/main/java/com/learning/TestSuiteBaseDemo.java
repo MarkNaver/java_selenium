37cc237c8b1988a108579fb1551dc44f2a70bb20
tcw <tcwaily@live.com>
2018-08-17 10:16:43 +0800

    优化合同系统case

--- src/main/java/com/learning/TestSuiteBaseDemo.java
+++ src/main/java/com/learning/TestSuiteBaseDemo.java
@@ -1,5 +1,7 @@
 package com.learning;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,10 +11,13 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
+import xxb.contract.ContractFactoryBase;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestSuiteBaseDemo {
+    private static final Logger log = LogManager.getLogger(ContractFactoryBase.class.getName());
+
     protected WebDriver driver;
 
 //    protected SearchPageFactoryDemoT search;
@@ -22,10 +27,15 @@ public class TestSuiteBaseDemo {
 
         // Browsers
         if (browser.equalsIgnoreCase("Chrome")) {
+            System.setProperty("webdriver.chrome.driver", "./src/main/java/Tools/chromedriver");
             driver = new ChromeDriver();
+            log.info("选择了Chrome浏览器。。。");
         } else if (browser.equalsIgnoreCase("Firefox")) {
+            System.setProperty("webdriver.gecko.driver", "./src/main/java/Tools/geckodriver");
             driver = new FirefoxDriver();
+            log.info("Firefox。。。");
         } else if (browser.equalsIgnoreCase("Chrome_headless")) {
+            //可能会报错。。。待解决
             ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.addArguments("--headless");
             driver = new ChromeDriver(chromeOptions);
