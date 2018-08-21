e944ab2dd8d8f6b0ac48e8648e052b646e8fa28e
tcw <tcwaily@live.com>
2018-08-21 10:04:53 +0800

    优化一些

--- src/main/java/com/config/selenium/Browser.java
+++ src/main/java/com/config/selenium/Browser.java
@@ -13,9 +13,10 @@ import java.util.concurrent.TimeUnit;
 
 public class Browser {
 
-    public String browserName;
-    public String serverURL;
+    private String browserName;
+    private String serverURL;
     public WebDriver driver;
+
 
     public void initConfigData() throws IOException{
 
@@ -24,11 +25,8 @@ public class Browser {
         InputStream ips = new FileInputStream("./src/main/java/com/config/selenium/config.properties");
         p.load(ips);
 
-        LOGGER.Output(LogType.LogTypeName.INFO, "开始从属性文件中选择浏览器名称");
         browserName=p.getProperty("browserName");
-        LOGGER.Output(LogType.LogTypeName.INFO, "选择的浏览器类型是: "+ browserName);
         serverURL = p.getProperty("URL");
-        LOGGER.Output(LogType.LogTypeName.INFO, "测试地址URL为: "+ serverURL);
         ips.close();
 
     }
@@ -40,17 +38,13 @@ public class Browser {
             System.setProperty("webdriver.gecko.driver", "./Tools/geckodriver.exe");
             driver = new FirefoxDriver();
 
-            LOGGER.Output(LogType.LogTypeName.INFO, "Launching Firefox ...");
 
         }else if(browserName.equalsIgnoreCase("Windows_Chrome")){
 
             System.setProperty("webdriver.chrome.driver", "src/main/java/Tools/chromedriver.exe");
             driver= new ChromeDriver();
-            LOGGER.Output(LogType.LogTypeName.INFO, "启动Chrome浏览器 ...");
             driver.get(serverURL);
-            LOGGER.Output(LogType.LogTypeName.INFO, "Open URL: "+ serverURL);
             driver.manage().window().maximize();
-            LOGGER.Output(LogType.LogTypeName.INFO, "最大化浏览器...");
             callWait(5);
             return driver;
 
@@ -58,17 +52,13 @@ public class Browser {
 
             System.setProperty("webdriver.ie.driver", "./Tools/IEDriverServer.exe");
             driver= new InternetExplorerDriver();
-            LOGGER.Output(LogType.LogTypeName.INFO, "启动 IE浏览器 ...");
         }else if (browserName.equalsIgnoreCase("Mac_Chrome"))
 
             System.setProperty("webdriver.chrome.driver", "./src/main/java/Tools/chromedriver");
             driver= new ChromeDriver();
-            LOGGER.Output(LogType.LogTypeName.INFO, "启动Chrome浏览器 ...");
 
         driver.get(serverURL);
-        LOGGER.Output(LogType.LogTypeName.INFO, "Open URL: "+ serverURL);
         driver.manage().window().maximize();
-        LOGGER.Output(LogType.LogTypeName.INFO, "最大化浏览器...");
         callWait(5);
         return driver;
     }
@@ -79,7 +69,6 @@ public class Browser {
 
     public void tearDown() throws InterruptedException{
 
-        LOGGER.Output(LogType.LogTypeName.INFO, "浏览器关闭中...");
         driver.quit();
         Thread.sleep(3000);
     }
@@ -87,10 +76,9 @@ public class Browser {
     /*
      * 隐式时间等待方法
      */
-    public void callWait(int time){
+    private void callWait(int time){
 
         driver.manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
-        LOGGER.Output(LogType.LogTypeName.INFO, "Wait for "+time+" seconds.");
     }
 
 
