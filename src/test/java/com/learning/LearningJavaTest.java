b2d10815324ae4c322ea68a0e84b60c88f59a89e
tcw <tcwaily@live.com>
2018-08-23 00:40:57 +0800

    POM.XML

--- src/test/java/com/learning/LearningJavaTest.java
+++ src/test/java/com/learning/LearningJavaTest.java
@@ -20,6 +20,7 @@ private static final Logger log = LogManager.getLogger(FrameworkTestCase.class.g
 
     @BeforeClass
     public void beforeClass() {
+        System.getProperty("webdriver.chrome.driver","./src/main/java/Tools/chromedriver");
         driver = new ChromeDriver();
         String baseUrl = "https://www.baidu.com/";
         driver.manage().window().maximize();
