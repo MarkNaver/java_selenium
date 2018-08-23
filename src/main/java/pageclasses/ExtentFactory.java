0385fd421787dac63db6de66dae89bdf333c20cc
tcw <tcwaily@live.com>
2018-08-23 20:02:05 +0800

    优化docker测试报告

--- src/main/java/pageclasses/ExtentFactory.java
+++ src/main/java/pageclasses/ExtentFactory.java
@@ -2,16 +2,21 @@ package pageclasses;
 
 import com.relevantcodes.extentreports.ExtentReports;
 
+import java.util.Properties;
+
 public class ExtentFactory {
     public static ExtentReports GetInstance() {
         //报告生成文件路径及系统信息等
+        Properties props = System.getProperties();
+        String osName = props.getProperty("os.name");
+        String userName = props.getProperty("user.name");
         ExtentReports extentReports;
         String Path = "./logs/report-demo.html";
         extentReports = new ExtentReports(Path, false);
         extentReports
                 .addSystemInfo("Selenium Version", "3.6.0")
-                .addSystemInfo("Platform", "Mac")
-                .addSystemInfo("测试执行人","Mr.陶")
+                .addSystemInfo("Platform", osName)
+                .addSystemInfo("测试执行人",userName)
                 .addSystemInfo("Browser","Chrome");
         return extentReports;
     }
