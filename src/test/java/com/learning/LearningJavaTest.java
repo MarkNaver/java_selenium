75ebfd5f606acc01f39b1b2ae320920919b38680
tcw <tcwaily@live.com>
2018-08-23 18:21:27 +0800

    graid

--- src/test/java/com/learning/LearningJavaTest.java
+++ src/test/java/com/learning/LearningJavaTest.java
@@ -3,27 +3,40 @@ package com.learning;//import com.config.selenium.LoggerDemoOne;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import testsuite.FrameworkTestCase;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 class LearningJavaTest {
     private WebDriver driver;
-
-    //        private static final Logger log = LogManager.getLogger
-private static final Logger log = LogManager.getLogger(FrameworkTestCase.class.getName());
+    private static final Logger log = LogManager.getLogger(FrameworkTestCase.class.getName());
 
     @BeforeClass
-    public void beforeClass() {
-        System.setProperty("webdriver.chrome.driver","/var/jenkins_home/workspace/java+selenium3/src/main/java/Tools/chromedriver");
-        driver = new ChromeDriver();
+    public void beforeClass() throws MalformedURLException {
+//        System.setProperty("webdriver.chrome.driver","/var/jenkins_home/workspace/java+selenium3/src/main/java/Tools/chromedriver");
+//        driver = new ChromeDriver();
+        String nodeURL = "http://47.94.100.252:4444/wd/hub";
+        WebDriver driver = null;
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setPlatform(Platform.UNIX);
+        caps = DesiredCapabilities.chrome();
+        caps.setVersion("68.0.3440.84");
+
+        driver = new RemoteWebDriver(new URL(nodeURL), caps);
+
         String baseUrl = "https://www.baidu.com/";
-        driver.manage().window().maximize();
+//        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
         driver.get(baseUrl);
         log.info("打开了浏览器");
