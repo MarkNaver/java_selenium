a613297c97a0ce5736c777347ee1d81c3ad6d095
tcwaily <tcwaily@me.com>
2018-10-17 14:30:05 +0800

    优化

---
+++ src/test/java/leaning/Puppy.java
@@ -0,0 +1,17 @@
+package leaning;
+
+/**
+ * @Author: tcwaily@me.com
+ * @Description:
+ * @Date: Created in 下午2:17 2018/10/17
+ * @Modified By:
+ */
+public class Puppy {
+    public Puppy(String name) {
+        System.out.println("小狗的名字是：" + name);
+    }
+
+    public static void main(String[] args) {
+        Puppy myPuppy = new Puppy("fdsfsfsd");
+    }
+}
